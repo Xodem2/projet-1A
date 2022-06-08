670adc12e556e8744618f00cca8a93016d903ca4
w-chan <wchan@enseirb-matmeca.fr>
2022-06-08 17:15:51 +0200

    liaison  jeu somme dans child_activity

---
+++ app/src/main/java/com/example/projet1a/SquareActivity.java
@@ -0,0 +1,17 @@
+package com.example.projet1a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class SquareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_square);
+    }
+
+
+}
