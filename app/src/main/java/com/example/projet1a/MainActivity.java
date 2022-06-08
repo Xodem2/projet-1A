62e9ee0f3a959240b9effba419e02e9ab773375d
w-chan <wchan@enseirb-matmeca.fr>
2022-06-08 09:59:44 +0200

    ajout des redirections TeenPage et ChildPage

--- app/src/main/java/com/example/projet1a/MainActivity.java
+++ app/src/main/java/com/example/projet1a/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private PlayerProfile player;
     private Button profileButton;
+    private Button teenButton;
+    private Button childButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +31,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         this.profileButton = (Button) findViewById(R.id.button_adult);
         this.profileButton.setOnClickListener(this);
+
+        this.teenButton = (Button) findViewById(R.id.button_teen);
+        this.teenButton.setOnClickListener(this);
+
+        this.childButton = (Button) findViewById(R.id.button_child);
+        this.childButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if(v.getId() == this.profileButton.getId()) this.showProfilePage();
+        if(v.getId() == this.teenButton.getId()) this.showTeenPage();
+        if(v.getId() == this.childButton.getId()) this.showChildPage();
     }
 
     private void showProfilePage() {
+        Intent profileActivityIntent = new Intent(this, ProfileActivity.class);
+        profileActivityIntent.putExtra(PLAYER_NICKNAME, this.player.getNickname());
+        profileActivityIntent.putExtra(PLAYER_AGE, this.player.getAge());
+        profileActivityIntent.putExtra(PLAYER_ID, this.player.getID());
+        startActivity(profileActivityIntent);
+    }
+
+    private void showTeenPage() {
+        Intent profileActivityIntent = new Intent(this, ProfileActivity.class);
+        profileActivityIntent.putExtra(PLAYER_NICKNAME, this.player.getNickname());
+        profileActivityIntent.putExtra(PLAYER_AGE, this.player.getAge());
+        profileActivityIntent.putExtra(PLAYER_ID, this.player.getID());
+        startActivity(profileActivityIntent);
+    }
+    private void showChildPage() {
         Intent profileActivityIntent = new Intent(this, ProfileActivity.class);
         profileActivityIntent.putExtra(PLAYER_NICKNAME, this.player.getNickname());
         profileActivityIntent.putExtra(PLAYER_AGE, this.player.getAge());
