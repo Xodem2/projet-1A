2f3df4cbe5fa18d5c698eba41da333376bb5f308
w-chan <wchan@enseirb-matmeca.fr>
2022-06-08 17:29:51 +0200

    icone ???

--- app/src/main/java/com/example/projet1a/SquareActivity.java
+++ app/src/main/java/com/example/projet1a/SquareActivity.java
@@ -3,7 +3,11 @@ package com.example.projet1a;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
 
 public class SquareActivity extends AppCompatActivity {
 
@@ -13,5 +17,24 @@ public class SquareActivity extends AppCompatActivity {
         setContentView(R.layout.activity_square);
     }
 
+    public class DrawView extends View {
+        Paint paint = new Paint();
 
+        public DrawView(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void onDraw(Canvas canvas) {
+            paint.setColor(Color.BLACK);
+            paint.setStrokeWidth(3);
+            canvas.drawRect(30, 30, 80, 80, paint);
+            paint.setStrokeWidth(0);
+            paint.setColor(Color.CYAN);
+            canvas.drawRect(33, 60, 77, 77, paint);
+            paint.setColor(Color.YELLOW);
+            canvas.drawRect(33, 33, 77, 60, paint);
+
+        }
+    }
 }
