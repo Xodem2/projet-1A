4a16254c971270d7516a2539b550faef739b6c76
w-chan <wchan@enseirb-matmeca.fr>
2022-06-08 15:46:02 +0200

    liaison  jeu somme dans child_activity

--- app/src/main/java/com/example/projet1a/ChildActivity.java
+++ app/src/main/java/com/example/projet1a/ChildActivity.java
@@ -9,24 +9,24 @@ import android.widget.Button;
 
 public class ChildActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button sommeButton;
-
+    private Button sommeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
-
-        this.sommeButton = (Button) findViewById(R.id.somme);
+        this.sommeButton = (Button) findViewById(R.id.button_somme);
         this.sommeButton.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == this.sommeButton.getId()) this.showSomme();
+        if(v.getId() == this.sommeButton.getId()) this.showSommePage();
     }
-
-    public void showSomme(){
+    private void showSommePage() {
         Intent sommeActivityIntent = new Intent(this, SommeActivity.class);
         startActivity(sommeActivityIntent);
     }
+
+
 }
