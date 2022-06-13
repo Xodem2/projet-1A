f3accb2425f0a0f4ad0f7984e4562f52a342677f
w-chan <wchan@enseirb-matmeca.fr>
2022-06-13 10:38:10 +0200

    update ProfileActivity

--- app/src/main/java/com/example/projet1a/ProfileActivity.java
+++ app/src/main/java/com/example/projet1a/ProfileActivity.java
@@ -16,7 +16,7 @@ import com.example.projet1a.profile.PlayerStatistics;
 import org.w3c.dom.Text;
 
 
-public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+public class ProfileActivity extends AppCompatActivity  {
 
     private PlayerProfile player;
 
@@ -43,66 +43,8 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         this.editAge = (EditText) findViewById(R.id.profilePageEditAgeId);
         this.playerId = (TextView) findViewById(R.id.profilePagePlayerIdFillId);
         this.updateButton = (Button) findViewById(R.id.profilePageUpdateButtonId);
-        this.spScore = (TextView) findViewById(R.id.profilePageSpScoreFillId);
-        this.mpScore = (TextView) findViewById(R.id.profilePageMpScoreFillId);
-        this.totalScore = (TextView) findViewById(R.id.profilePageTotalScoreFillId);
+
         this.gameStatsButton = (Button) findViewById(R.id.profilePageGameStatsButtonId);
-        this.level = (TextView) findViewById(R.id.profilePageLevelFillId);
-        this.currentXp = (TextView) findViewById(R.id.profilePageCurrentXpFillId);
-        this.neededXp = (TextView) findViewById(R.id.profilePageNeededXpFillId);
-
-        this.updateButton.setOnClickListener(this);
-        this.gameStatsButton.setOnClickListener(this);
-
-        this.showPlayerInfo();
-        this.showPlayerStats();
-        this.showPlayerLevel();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        this.showPlayerInfo();
-        this.showPlayerStats();
-        this.showPlayerLevel();
-    }
-
-    @Override
-    public void onClick(View v) {
-        if(v.getId() == this.updateButton.getId()) this.updatePlayer();
-        if(v.getId() == this.gameStatsButton.getId()) this.showGameStats();
-    }
-
-    private void showGameStats() {
 
     }
-
-    private void updatePlayer() {
-        this.player.setNickname(this.editNickname.getText().toString());
-        this.player.setAge(Integer.parseInt(this.editAge.getText().toString()));
-
-        // update database
-        DataProvider.getInstance().getMyLocalDatabase().savePlayer(this.player);
-    }
-
-    private void showPlayerInfo(){
-        this.playerId.setText(this.player.getID());
-        this.editNickname.setText(this.player.getNickname());
-        this.editAge.setText(String.valueOf(this.player.getAge()));
-    }
-
-    private void showPlayerStats(){
-        this.spScore.setText(String.valueOf(this.player.getStats().getSingleplayerScore()));
-        this.mpScore.setText(String.valueOf(this.player.getStats().getMultiplayerScore()));
-        this.totalScore.setText(String.valueOf(this.player.getStats().getTotalScore()));
-    }
-
-    private void showPlayerLevel(){
-        this.level.setText(String.valueOf(this.player.getLevel().getLevel()));
-        this.currentXp.setText(String.valueOf(this.player.getLevel().getCurrentXp()));
-        this.neededXp.setText(String.valueOf(this.player.getLevel().getNeededXp()));
-
-        Log.v("profileActivity", String.valueOf(this.player.getLevel().getCurrentXp()));
-    }
-
 }
