eb8d22861a0a7dc298473b11c55670df9701b1b5
maximekuhn <pingouin2032.maxime@gmail.com>
2022-06-13 11:35:29 +0200

    affichage succes

--- app/src/main/java/com/example/projet1a/SuccessActivity.java
+++ app/src/main/java/com/example/projet1a/SuccessActivity.java
@@ -1,14 +1,32 @@
 package com.example.projet1a;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.projet1a.profile.PlayerProfile;
+
 public class SuccessActivity extends AppCompatActivity {
+
+    private PlayerProfile player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
+
+        this.player = DataProvider.getInstance().getPlayer();
+
+        String[] successId = new String[this.player.getSuccess().getAllSuccess().size()];
+        for(int i = 0; i < this.player.getSuccess().getAllSuccess().size(); i++){
+            successId[i] = this.player.getSuccess().getAllSuccess().valueAt(i).getId();
+        }
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.successPageRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        CustomAdapter customAdapter = new CustomAdapter(successId);
+        recyclerView.setAdapter(customAdapter);
     }
 }
