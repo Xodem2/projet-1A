63a3cdd93773c8329d4bf7bea991af82ad02be35
maximekuhn <pingouin2032.maxime@gmail.com>
2022-06-13 14:55:15 +0200

    xpbar fonctionnelle

--- app/src/main/java/com/example/projet1a/ProfileActivity.java
+++ app/src/main/java/com/example/projet1a/ProfileActivity.java
@@ -2,31 +2,29 @@ package com.example.projet1a;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.example.projet1a.profile.PlayerLevel;
 import com.example.projet1a.profile.PlayerProfile;
 
 
-public class ProfileActivity extends AppCompatActivity  {
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
     private PlayerProfile player;
-
-    private EditText editNickname;
-    private EditText editAge;
-    private TextView playerId;
+    private EditText nickname;
+    private EditText age;
+    private TextView playerIdFill;
     private Button updateButton;
-    private TextView spScore;
-    private TextView mpScore;
-    private TextView totalScore;
-    private TextView level;
-    private TextView currentXp;
-    private TextView neededXp;
-    private Button gameStatsButton;
-    private ImageButton succesButton;
+    private ProgressBar xpBar;
+    private ImageButton successButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +33,46 @@ public class ProfileActivity extends AppCompatActivity  {
 
         this.player = DataProvider.getInstance().getPlayer();
 
-        this.editNickname = (EditText) findViewById(R.id.profilePageEditNicknameId);
-        this.editAge = (EditText) findViewById(R.id.profilePageEditAgeId);
-        this.playerId = (TextView) findViewById(R.id.profilePagePlayerIdFillId);
+        this.nickname = (EditText) findViewById(R.id.profilePageEditNicknameId);
+        this.age = (EditText) findViewById(R.id.profilePageEditAgeId);
+        this.playerIdFill = (TextView) findViewById(R.id.profilePagePlayerIdFillId);
         this.updateButton = (Button) findViewById(R.id.profilePageUpdateButtonId);
+        this.xpBar = (ProgressBar) findViewById(R.id.profilePageProgressBarId);
+        this.successButton = (ImageButton) findViewById(R.id.profilePageSuccesButtonId);
 
-        this.gameStatsButton = (Button) findViewById(R.id.profilePageGameStatsButtonId);
+        this.updateButton.setOnClickListener(this);
+        this.successButton.setOnClickListener(this);
+    }
 
-        this.succesButton = (ImageButton) findViewById(R.id.profilePageSuccesButtonId);
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == this.updateButton.getId()) this.doUpdate();
+        if(v.getId() == this.successButton.getId()) this.showSuccessPage();
+    }
 
+    @Override
+    public void onResume(){
+        super.onResume();
+        this.showPlayerInfo();
+    }
+
+    private void doUpdate() {
+        this.player.setNickname(this.nickname.getText().toString());
+        this.player.setAge(Integer.parseInt(this.age.getText().toString()));
+        DataProvider.getInstance().getMyLocalDatabase().savePlayer(this.player);
+    }
+
+    private void showPlayerInfo(){
+        this.nickname.setText(this.player.getNickname());
+        this.age.setText(String.valueOf(this.player.getAge()));
+        this.playerIdFill.setText(this.player.getID());
+
+        PlayerLevel level = this.player.getLevel();
+        int progress = (int) ((float) level.getCurrentXp() * 100 / level.getNeededXp());
+        this.xpBar.setProgress(progress);
+    }
+
+    private void showSuccessPage(){
+        startActivity(new Intent(this, SuccessActivity.class));
     }
 }
