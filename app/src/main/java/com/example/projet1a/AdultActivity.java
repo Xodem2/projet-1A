17522bafc4f585bb059683770dda58662910e5f6
LifeTakerSimo <lifestaker.simo@gmail.com>
2022-06-13 17:51:17 +0200

    background for adult page

--- app/src/main/java/com/example/projet1a/AdultActivity.java
+++ app/src/main/java/com/example/projet1a/AdultActivity.java
@@ -3,10 +3,13 @@ package com.example.projet1a;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.VideoView;
 
 import com.example.projet1a.profile.PlayerProfile;
 
@@ -14,6 +17,9 @@ public class AdultActivity extends AppCompatActivity implements View.OnClickList
 
     private ImageButton vectorButton;
     private ImageButton matriceButton;
+    MediaPlayer mMediaPlayer;
+    int mCurrentVideoPosition;
+    private VideoView adultback;
 
     private PlayerProfile player;
 
@@ -29,8 +35,49 @@ public class AdultActivity extends AppCompatActivity implements View.OnClickList
 
         this.matriceButton = (ImageButton) findViewById(R.id.icone_Matrice);
         this.matriceButton.setOnClickListener(this);
+        adultback = (VideoView) findViewById(R.id.adultback);
+        String uriPath = "android.resource://"+getPackageName()+"/"+R.raw.adultback;
+        Uri uri = Uri.parse(uriPath);
+        adultback.setVideoURI(uri);
+        adultback.start();
+        adultback.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mMediaPlayer = mediaPlayer;
+                // We want our video to play over and over so we set looping to true.
+                mMediaPlayer.setLooping(true);
+                // We then seek to the current posistion if it has been set and play the video.
+                if (mCurrentVideoPosition != 0) {
+                    mMediaPlayer.seekTo(mCurrentVideoPosition);
+                    mMediaPlayer.start();
+                }
+            }
+        });
+
+    }
+    @Override
+    protected void onPostResume() {
+        adultback.resume();
+        super.onPostResume();
     }
 
+    @Override
+    protected void onRestart() {
+        adultback.start();
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        adultback.suspend();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        adultback.stopPlayback();
+        super.onDestroy();
+    }
     @Override
     public void onClick(View v) {
         if(v.getId() == this.vectorButton.getId()) this.showVectorActivity();
